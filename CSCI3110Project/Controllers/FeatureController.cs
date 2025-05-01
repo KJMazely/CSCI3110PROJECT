c60efe25b832719b32eadf2ce99c62602ad337eb
MehZehLee <151669416+MehZehLee@users.noreply.github.com>
2025-05-01 06:20:56 +0000

    Update FeatureController.cs

--- CSCI3110Project/Controllers/FeatureController.cs
+++ CSCI3110Project/Controllers/FeatureController.cs
@@ -1,23 +1,104 @@
-﻿// RESTful API for Vehicle Features
-
+// RESTful API for Vehicle Features
+using System.Threading.Tasks;
 using KMCSCI3110Project.Data;
+using KMCSCI3110Project.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-[Authorize(Roles = "Admin")]
-public class FeatureController : Controller
+namespace KMCSCI3110Project.Controllers
 {
-    private readonly ApplicationDbContext _db;
-    public FeatureController(ApplicationDbContext db) => _db = db;
-
-    // GET: /Feature/GetAll
-    [HttpGet]
-    public async Task<IActionResult> GetAll()
+    [Authorize(Roles = "Admin")]
+    public class FeatureController : Controller
     {
-        var list = await _db.Features
-            .Select(f => new { f.Id, f.Name })
-            .ToListAsync();
-        return Json(list);
+        private readonly ApplicationDbContext _db;
+
+        public FeatureController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: /Feature
+        public async Task<IActionResult> Index()
+        {
+            var list = await _db.Features.ToListAsync();
+            return View(list);
+        }
+
+        // GET: /Feature/Create
+        public IActionResult Create()
+        {
+            return View(new Feature());
+        }
+
+        // GET: /Feature/GetAll
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var list = await _db.Features
+                .Select(f => new { f.Id, f.Name })
+                .ToListAsync();
+            return Json(list);
+        }
+
+        // POST: /Feature/Create
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Feature feature)
+        {
+            if (!ModelState.IsValid)
+                return View(feature);
+
+            _db.Features.Add(feature);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: /Feature/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var feature = await _db.Features.FindAsync(id);
+            if (feature == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(feature);
+        }
+
+        // POST: /Feature/Edit/5
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Feature feature)
+        {
+            if (id != feature.Id)
+                return RedirectToAction(nameof(Index));
+
+            if (!ModelState.IsValid)
+                return View(feature);
+
+            _db.Features.Update(feature);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: /Feature/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var feature = await _db.Features.FindAsync(id);
+            if (feature == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(feature);
+        }
+
+        // POST: /Feature/Delete/5
+        [HttpPost, ActionName("Delete"), ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var feature = await _db.Features.FindAsync(id);
+            if (feature != null)
+            {
+                _db.Features.Remove(feature);
+                await _db.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
